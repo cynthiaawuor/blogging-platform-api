574f12574d93eb8f025f66dd38db93e0517697e4
cynthiaawuor <cynthiaawuor2000@gmail.com>
2026-03-23 11:19:17 +0300

    adding REDME file

---
+++ README.md
@@ -0,0 +1,26 @@
+## blogging-platform-api
+
+### Project setup
+
+Requirements
+
+- node
+- express
+- typescript
+
+Clone the repository
+
+```bash
+git clone git@github.com:cynthiaawuor/blogging-platform-api.git
+
+cd blogging-platform-api
+
+//Install dependencies
+npm install
+```
+
+Start the server
+
+```bash
+npm run start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
-export default const app = express();
+const app = express();
 const PORT = 3000;
 
 //JSON parser middleware
 app.use(express.json());
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
 });
